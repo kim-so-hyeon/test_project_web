1242424ab243831bfb829b19c94373ac52bc722f
kim-so-hyeon <ssojja0987@gmail.com>
2020-05-14 17:39:01 +0900

    first commit

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.http import JsonResponse
-from packages.class_kimsohyeon import Cal3 as C3
+# from packages.class_kimsohyeon import Cal3 as C3
 
 def home(request):
     value01 = request.GET['value1']
     value02 = request.GET['value2']
-    val3 = C3(4,6)
+    # val3 = C3(4,6)
     import pickle
-    pickle.dump(val3, open("./pickle_kimsohyeon.pkl","wb"))
+    # pickle.dump(val3, open("./pickle_kimsohyeon.pkl","wb"))
     val_pkl = pickle.load(open("./pickle_kimsohyeon.pkl","rb"))
     result = val_pkl.getPredict()
     requestDict = {'result_response':result}
