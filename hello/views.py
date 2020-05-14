2f8dcce8c992e2199e0d649eecb6453293c92c3d
kim-so-hyeon <ssojja0987@gmail.com>
2020-05-14 15:36:19 +0900

    first commit

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! & Hello ssoja~ Have a nice day!")
+    return HttpResponse("Hello, Django!")
 # Create your views here.
