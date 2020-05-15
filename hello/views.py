53c765873c4db2646797ab3bda2c38c7fd6cdac2
kim-so-hyeon <ssojja0987@gmail.com>
2020-05-15 09:40:46 +0900

    first commit

--- hello/views.py
+++ hello/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from django.http import JsonResponse
-from packages.class_kimsohyeon import Cal3 as C3
+import pickle
+
+class Cal3:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def getPredict(self):
+        c = int(self.x) + int(self.y)
+        return c
 
 def home(request):
     value01 = request.GET['value1']
     value02 = request.GET['value2']
-    # val3 = C3(4,6)
-    import pickle
-    # pickle.dump(val3, open("./pickle_kimsohyeon.pkl","wb"))
-    val_pkl = pickle.load(open("./pickle_kimsohyeon.pkl","rb"))
+    val3 = Cal3(value01,value02)
+    pickle.dump(val3, open("D:/workspace/test_project_web/pickle_kimsohyeon.pkl","wb"))
+    with open("D:/workspace/test_project_web/pickle_kimsohyeon.pkl","rb") as pc :
+        val_pkl = pickle.load(pc)
     result = val_pkl.getPredict()
     requestDict = {'result_response':result}
     return JsonResponse(requestDict)
